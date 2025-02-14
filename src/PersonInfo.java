0292aac02388dd82aef74ddd3e148d89c28f64be
Jonathan <Jonathan@Greg>
2025-02-14 13:59:04 -0700

    Initial commit: Added PersonInfo.java

---
+++ src/PersonInfo.java
@@ -0,0 +1,11 @@
+public class PersonInfo {
+	public static void main(String[] args) {
+		System.out.println("First Name: Jonathan");
+		System.out.println("Last Name: Busch");
+		System.out.println("Street Address: 12345 Eclypse Avenue");
+		System.out.println("City: Denver");
+		System.out.println("Zip Code: 80124");
+
+	}
+
+}
